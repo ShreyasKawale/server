a1f652cf0af28bbc04ae65ce20005c59fcdda421
ShreyasKawale <shreyaskawale7t@gmail.com>
2024-09-07 15:45:04 +0530

    Configure postgres database

--- HubSquad/settings.py
+++ HubSquad/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'HubSquad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgres',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
